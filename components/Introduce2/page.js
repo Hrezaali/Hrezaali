77f182da3962cc8cff2f59c7725404b4ad591ad1
Hassan Rezaali <hassan.rezaali@gmail.com>
2025-03-09 05:08:50 +0330

    Replace all files with new ones

--- components/Introduce2/page.js
+++ components/Introduce2/page.js
@@ -3,10 +3,10 @@ import React from 'react';
 export default function Introduce2() {
   return (
     <div className="mb-40">
-      <span className="block text-2xl font-semibold text-center mt-4 text-gray-300">
+      <span className="block text-2xl font-semibold text-center mt-4 text-black-600 dark:text-white-300">
         Hello, I'm Hassan Rezaali, a passionate Software Engineer from Iran.
       </span>
-      <span className="block text-xl md:text-2xl font-semibold text-center text-gray-200 mt-2">
+      <span className="block text-xl md:text-2xl font-semibold text-center text-black-600 dark:text-white-300 mt-2">
         I am a specialized Web Developer with expertise in Front-End development and Next.js.
       </span>
     </div>
